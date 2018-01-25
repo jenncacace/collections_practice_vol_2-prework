dc7787e9310ffa24f3fb4589e269b5aaa4f32734
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-25 19:38:54 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,65 @@
-# your code goes here
+def begins_with_r(collection)
+  collection.each do |word|
+    if word[0] != "r"
+      return false
+    end 
+  end 
+  return true 
+end
+
+def contain_a(collection)
+  collection.select do |word|
+    word.include?("a")
+  end 
+end   
+
+def first_wa(collection)
+  collection.find do |word|
+    word[0..1] == "wa"
+  end 
+end
+
+def remove_non_strings(collection)
+  new_collection = []
+  collection.find do |value|
+    if value.class == String
+      new_collection << value
+    end 
+  end 
+  new_collection
+end 
+
+
+def count_elements(collection)
+names = []
+  collection.each do |hash|
+    names << hash[:name] 
+  end 
+  collection.each do |hash|
+    hash[:count] = names.count(hash[:name])
+  end.uniq
+end
+
+def merge_data(keys, data)
+new_array = []
+  data[0].each do |name, attributes|
+    attributes[:first_name] = name 
+    new_array << attributes 
+  end
+  new_array
+end
+
+
+def find_cool(collection)
+cool = []
+  collection.each do |hash|
+    if hash[:temperature] == "cool"
+      cool << hash 
+    end
+  end
+  cool
+end
+
+#come back
+def organize_schools(collection)
+end 
